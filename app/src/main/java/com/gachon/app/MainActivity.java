245b25acccb57893f33fd4db01fc479ff00270e3
garynoh <sw79899@gmail.com>
2017-04-12 03:26:55 +0900

    DrawerLayout 을 적용

--- app/src/main/java/com/gachon/app/MainActivity.java
+++ app/src/main/java/com/gachon/app/MainActivity.java
@@ -1,23 +1,18 @@
 package com.gachon.app;
 
-/**
- * Created by garyNoh on 17. 1. 26..
- * 메인화면
- * 1. 잠금/열림 버튼
- * 2. 자동/수동 모드
- * 3. 배터리
- * 4. drawer 메뉴
- *
- * reference : https://github.com/wasabeef/awesome-android-ui
- **/
-
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -28,7 +23,7 @@ import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
 
     public static String TAG = "tag";
@@ -50,7 +45,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //서비스가 바인딩된 후 controlactivity 가 생성
+        setContentView(R.layout.activity_main_drawer); //서비스가 바인딩된 후 controlactivity 가 생성
 
         //17.2.7 : 특정 탭 모양에 따른 enum value 가져오기
         TabEnum tabEnum = getDemo();
@@ -62,6 +57,19 @@ public class MainActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         //TODO gachon : delete menu
         invalidateOptionsMenu();
+
+
+        //drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
 
         //17.2.7 : 하단 탭 설정
         ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
@@ -119,6 +127,64 @@ public class MainActivity extends AppCompatActivity{
 
     private TabEnum getDemo() {
         return TabEnum.values()[0];
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
