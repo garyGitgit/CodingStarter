e3adb9ff5582a5938713003a3e9e979451b229dc
garynoh <sw79899@gmail.com>
2017-04-30 12:37:45 +0900

    add cardview on course1step1.xml

---
+++ app/src/main/java/com/gachon/app/Course1Activity.java
@@ -0,0 +1,96 @@
+package com.gachon.app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Course1Activity extends AppCompatActivity {
+    ViewPager course1ViewPager;
+    static int courseStepNum = 5;
+    ImageView[] progressImageViewList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course1);
+
+        //progress 상태 표시
+        progressImageViewList = new ImageView[5];
+        progressImageViewList[0] = (ImageView)findViewById(R.id.course_progress1);
+        progressImageViewList[1] = (ImageView)findViewById(R.id.course_progress2);
+        progressImageViewList[2] = (ImageView)findViewById(R.id.course_progress3);
+        progressImageViewList[3] = (ImageView)findViewById(R.id.course_progress4);
+        progressImageViewList[4] = (ImageView)findViewById(R.id.course_progress5);
+
+
+        //초기 시작은 첫번째 progress 이미지 초기화
+        progressImageViewList[0].setBackgroundColor(Color.GREEN);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int thisPage = course1ViewPager.getCurrentItem();
+
+                if (thisPage < 4) {
+                    Toast.makeText(Course1Activity.this, "성공!", Toast.LENGTH_SHORT).show();
+                    course1ViewPager.setCurrentItem(thisPage + 1);
+                    //지금 페이지 번호에 맞게 progress 배경색을 색칠해준다. 추후에는 색깔을 칠하던가 색깔있는 아이콘을 쓰던가 해야지
+                    for (int i = 0 ; i <= thisPage+1; i++){
+                        progressImageViewList[i].setBackgroundColor(Color.GREEN);
+                    }
+                }
+                else
+                    Toast.makeText(Course1Activity.this, "마지막 단계입니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        course1ViewPager = (ViewPager) findViewById(R.id.course1ViewPager);
+        course1ViewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        course1ViewPager.setCurrentItem(0);
+
+    }
+
+
+    private class PagerAdapter extends FragmentStatePagerAdapter {
+        public PagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new Course1Step1Fragment();
+                case 1:
+                    return new Course1Step2Fragment();
+                case 2:
+                    return new Course1Step3Fragment();
+                case 3:
+                    return new Course1Step4Fragment();
+                case 4:
+                    return new Course1Step5Fragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return courseStepNum;
+        }
+    }
+
+}
--- app/src/main/java/com/gachon/app/Fragment1.java
+++ app/src/main/java/com/gachon/app/Fragment1.java
@@ -13,91 +13,92 @@ import android.widget.ImageView;
 
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
 
-public class Fragment1 extends Fragment implements MainActivity.onBluetoothMessageReceived{
-  View rootView;
+public class Fragment1 extends Fragment implements MainActivity.onBluetoothMessageReceived {
+    View rootView;
 
-  ImageView imageViewMainBlock1_1;
-
-  /*
-  fragment 1 : 메인 페이지
-   */
-  public static final String TAG = "gachon/Fragment1/";
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                           @Nullable Bundle savedInstanceState) {
-    int position = FragmentPagerItem.getPosition(getArguments());
-    Log.e("bisecu", "onCreateView : " + Integer.toString(position));
-    setRetainInstance(true);
-    rootView = inflater.inflate(R.layout.fragment_fragment1, container, false);
-    imageViewMainBlock1_1 = (ImageView)rootView.findViewById(R.id.mainBlock1_1);
-    imageViewMainBlock1_1.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        startActivity(new Intent(rootView.getContext(), Main2Activity.class));
-      }
-    });
-
-    return rootView;
-  }
+    ImageView imageViewMainBlock1_1;
 
 
-  @Override
-  public void onStop() {
-    Log.e("bisecu", "onStop");
-    super.onStop();
-  }
+    /*
+    fragment 1 : 메인 페이지
+     */
+    public static final String TAG = "gachon/Fragment1/";
 
-  @Override
-  public void onResume() {
-    Log.e("bisecu", "onResume");
-    super.onResume();
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        int position = FragmentPagerItem.getPosition(getArguments());
+        Log.e("bisecu", "onCreateView : " + Integer.toString(position));
+        setRetainInstance(true);
+        rootView = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        imageViewMainBlock1_1 = (ImageView) rootView.findViewById(R.id.mainBlock1_1);
+        imageViewMainBlock1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(rootView.getContext(), Course1Activity.class));
+            }
+        });
 
-  @Override
-  public void onPause() {
-    Log.e("bisecu", "onPause");
-    super.onPause();
-  }
+        return rootView;
+    }
 
-  @Override
-  public void onDetach() {
-    Log.e("bisecu", "onDetach");
-    super.onDetach();
-  }
 
-  @Override
-  public void onDestroy() {
-    Log.e("bisecu", "onDestroy");
-    super.onDestroy();
-  }
+    @Override
+    public void onStop() {
+        Log.e("bisecu", "onStop");
+        super.onStop();
+    }
 
-  @Override
-  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-    Log.e("bisecu", "onActivityCreated");
-    super.onActivityCreated(savedInstanceState);
-  }
+    @Override
+    public void onResume() {
+        Log.e("bisecu", "onResume");
+        super.onResume();
+    }
 
-  @Override
-  public void onAttach(Activity activity) {
-    Log.e("bisecu", "onAttach");
-    super.onAttach(activity);
-  }
+    @Override
+    public void onPause() {
+        Log.e("bisecu", "onPause");
+        super.onPause();
+    }
 
-  @Override
-  public void onStart() {
-    Log.e("bisecu", "onStart");
-    super.onStart();
-  }
+    @Override
+    public void onDetach() {
+        Log.e("bisecu", "onDetach");
+        super.onDetach();
+    }
 
-  @Override
-  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    Log.e("bisecu", "onViewCreated");
-    super.onViewCreated(view, savedInstanceState);
-  }
+    @Override
+    public void onDestroy() {
+        Log.e("bisecu", "onDestroy");
+        super.onDestroy();
+    }
 
-  @Override
-  public void onMessageReceived(byte[] receivedData) {
-  }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.e("bisecu", "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        Log.e("bisecu", "onAttach");
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onStart() {
+        Log.e("bisecu", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Log.e("bisecu", "onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onMessageReceived(byte[] receivedData) {
+    }
 
 }
