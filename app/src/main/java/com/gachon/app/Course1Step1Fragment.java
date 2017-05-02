527fbfa8b0daf88ff0d9124ffcbb0d91fe08fb4b
garynoh <sw79899@gmail.com>
2017-05-02 22:41:42 +0900

    update course1step1fragment.java

--- app/src/main/java/com/gachon/app/Course1Step1Fragment.java
+++ app/src/main/java/com/gachon/app/Course1Step1Fragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +14,8 @@ import android.view.ViewGroup;
  */
 public class Course1Step1Fragment extends Fragment {
 
+    Button buttonGoNext;
+    View root;
 
     public Course1Step1Fragment() {
         // Required empty public constructor
@@ -23,7 +26,16 @@ public class Course1Step1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course1_step1, container, false);
+        root = inflater.inflate(R.layout.fragment_course1_step1, container, false);
+
+        buttonGoNext = (Button)root.findViewById(R.id.buttonGoNext);
+        buttonGoNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return root;
     }
 
 }
